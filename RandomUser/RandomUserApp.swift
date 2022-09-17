f9d8a7c1abf334d0156b0051a6c19affd1b33369
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-09-17 17:09:37 +0100

    Initial Commit

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by Vijayaraghavan Sundararaman on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
